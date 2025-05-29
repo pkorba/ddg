cfb213f5f91ef17b17f51019da129a418b702c7e
pkorba <32198274+pkorba@users.noreply.github.com>
2025-05-29 13:57:24 +0200

    Cleanup code, add request headers

--- ddg.py
+++ ddg.py
@@ -4,7 +4,7 @@ from maubot.handlers import command
 
 
 class DdgBot(Plugin):
-    @command.new(name="s", help="DuckDuckGo Search")
+    @command.new(name="s", help="Get the most relevant result from DuckDuckGo Web Search")
     @command.argument("query", pass_raw=True, required=True)
     async def search(self, evt: MessageEvent, query: str) -> None:
         await evt.mark_read()
@@ -14,20 +14,24 @@ class DdgBot(Plugin):
             return
         url = await self.web_search(query)
         if not url:
-            await evt.reply(f"Failed to find results for {query}")
+            await evt.reply(f"Failed to find results for *{query}*")
             return
         await evt.reply(f"{url}")
 
     async def web_search(self, query: str) -> str:
+        headers = {
+            "Sec-GPC": "1",
+            "accept-encoding": "gzip, deflate, br, zstd",
+            "accept-language": "pl,en-US;q=0.7,en;q=0.3",
+            "user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:138.0) Gecko/20100101 Firefox/138.0"
+        }
         url = f"https://lite.duckduckgo.com/lite/?q=\\+{query}"
         try:
-            timeout = aiohttp.ClientTimeout(total=7)
-            async with aiohttp.ClientSession(timeout=timeout) as session:
-                async with session.get(url, allow_redirects=True) as response:
-                    result_url = str(response.url)
-                    if "duckduckgo.com" in result_url:
-                        return ""
-                    return result_url
+            timeout = aiohttp.ClientTimeout(total=20)
+            response = await self.http.get(url, headers=headers, allow_redirects=True, timeout=timeout, raise_for_status=True)
+            result = str(response.url)
+            if "duckduckgo.com" not in result:
+                return result
         except aiohttp.ClientError as e:
-            self.log.error(f"Connection failed: {url}: {e}")
-            return ""
+            self.log.error(f"Connection failed: {e}")
+        return ""
