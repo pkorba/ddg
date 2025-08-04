25058ca1605b76cc5aac47553b8699cbf46da9db
pkorba <32198274+pkorba@users.noreply.github.com>
2025-08-04 14:44:03 +0200

    Update user-agent

--- ddg/ddg.py
+++ ddg/ddg.py
@@ -51,7 +51,7 @@ class DdgBot(Plugin):
             "Sec-GPC": "1",
             "accept-encoding": "gzip, deflate, br, zstd",
             "accept-language": "en,en-US;q=0.5",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:139.0) Gecko/20100101 Firefox/139.0",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0",
             "referer": "https://duckduckgo.com/"
         }
         vqd = await self.get_vqd(query)
